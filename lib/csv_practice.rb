aff3dab526e8e4aac826c3aacf2c250cd87f5d9e
Eve Le <nguyenthao.lee@gmail.com>
2019-08-26 21:52:48 -0700

    Implemented csv_practice.rb and tests

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -4,31 +4,56 @@ require 'awesome_print'
 
 # Part 1 - CSV Practice
 def load_data(filename)
-
+    return CSV.read(filename, headers: true).map { |line| line.to_h }
 end
 
 def total_medals_per_country(olympic_data)
-
+    output = Array.new
+    olympic_data.each do |hash|
+        index = output.find_index { |element| element[:country] == hash["Team"]}
+        if index
+            if hash["Medal"] != "NA"
+                output[index][:count] += 1
+            end 
+        else
+            new_country = {country: hash["Team"], count: 0}
+            if hash["Medal"] != "NA"
+                new_country[:count] += 1
+            end
+            output << new_country
+        end
+    end
+    return output  
 end
 
 def save_medal_totals(filename, medal_totals)
-
+    CSV.open(filename, 'w') do |file|
+        medal_totals.each do |country|
+            file << [country[:country], country[:count]]
+        end
+    end
 end
 
 # Part 2 - More Enumerable Practice
 
 def all_gold_medal_winners(olympic_data)
-
+    return olympic_data.select { |element| element["Medal"] == "Gold" }
 end
 
-def metals_sorted_by_country(metal_totals)
-
+def medals_sorted_by_country(medal_totals)
+    return medal_totals.sort_by { |element| element[:country] }
 end
 
-def country_with_most_metals(metal_totals)
-
+def country_with_most_medals(medal_totals)
+    return medal_totals.max_by { |hash| hash[:count] }
 end
 
 def athlete_height_in_inches(olympic_data)
-
+    output = Array.new
+    olympic_data.each do |element|
+        new_information = element.dup
+        new_information["Height"] =  new_information["Height"].to_i / 2.5
+        output << new_information 
+    end
+    return output
 end
--- test/csv_practice_test.rb
+++ test/csv_practice_test.rb
@@ -16,33 +16,33 @@ OLYMPIC_DATA_FILENAME = 'data/athlete_events.csv'
 
 describe 'CSV Practice Methods' do
   describe 'load_data' do
-    it 'returns an array of hashes' do
+    it 'returns an array of hashes' do   
       # Arrange & Act
       data = load_data(OLYMPIC_DATA_FILENAME)
-
+      
       # Assert
       expect(data.class).must_equal Array
       data.each do |data_row|
         expect(data_row.class).must_equal Hash
-
+        
         CSV_HEADERS.each do |key|
           expect(data_row.key?(key)).must_equal true
         end
       end
     end
-
+    
     it 'has the proper number of rows' do
       # Arrange & Act
       data = load_data(OLYMPIC_DATA_FILENAME)
-
+      
       # Assert
       expect(data.length).must_equal 49503
     end
-
+    
     it 'has the right 1st and last row' do
       # Arrange & Act
       data = load_data(OLYMPIC_DATA_FILENAME)
-
+      
       # Assert
       expect(data.first['ID']).must_equal '21'
       expect(data.first['Name']).must_equal 'Ragnhild Margrethe Aamodt'
@@ -52,11 +52,11 @@ describe 'CSV Practice Methods' do
       expect(data.last['Team']).must_equal 'Belarus'
     end
   end
-
+  
   describe 'total_medals_per_country' do
     # Arrange
     data = load_data(OLYMPIC_DATA_FILENAME)
-
+    
     correct_totals = {
       'Norway' => 133,
       'United States' => 944,
@@ -67,45 +67,45 @@ describe 'CSV Practice Methods' do
       'Jamaica' => 69,
       'United Arab Emirates' => 1
     }
-
+    
     it 'should return an array of hashes' do
-
+      
       # Act
       total_medals = total_medals_per_country(data)
-
+      
       # Assert
       expect(total_medals).must_be_instance_of Array
       correct_totals.keys.each do |country|
         country_total = total_medals.find do |medal_total|
-          medal_total["country"] == country
+          medal_total[:country] == country
         end
-
-      expect(country_total["count"]).must_equal correct_totals[country]
+        
+        expect(country_total[:count]).must_equal correct_totals[country]
       end
     end
   end
-
+  
   describe 'Can create a total medal file' do
     it 'The file is created' do
       # Arrange
       data = load_data(OLYMPIC_DATA_FILENAME)
       medal_totals = total_medals_per_country(data)
-
+      
       # Act
       save_medal_totals(MEDAL_TOTALS_FILENAME, medal_totals)
-
+      
       # Assert
       expect(File.exist?(MEDAL_TOTALS_FILENAME)).must_equal true
     end
-
+    
     it 'The file contains the right entries and headers' do
       # Arrange
       data = load_data(OLYMPIC_DATA_FILENAME)
       medal_totals = total_medals_per_country(data)
-
+      
       # Act
       save_medal_totals(MEDAL_TOTALS_FILENAME, medal_totals)
-
+      
       # Assert
       CSV.read(OLYMPIC_DATA_FILENAME, headers: true) do |line|
         correct_totals.keys.each do |key|
@@ -114,30 +114,30 @@ describe 'CSV Practice Methods' do
       end
     end
   end
-
+  
   describe 'All Gold Medal Winners' do
     # Arrange
     data = load_data(OLYMPIC_DATA_FILENAME)
-
+    
     # Act
     gold_medal_winners = all_gold_medal_winners(data)
-
+    
     it 'returns an array of hashes' do
-
+      
       # Assert
       expect(gold_medal_winners.class).must_equal Array
       gold_medal_winners.each do |winner|
         expect(winner.class).must_equal Hash
       end
     end
-
+    
     it 'They all have `Gold` in the medal field' do
       # Assert
       gold_medal_winners.each do |winner|
         expect(winner['Medal'].upcase.include? 'GOLD').must_equal true
       end
     end
-
+    
     it 'has the correct number of gold medalists' do
       count = data.sum do |athlete|
         athlete['Medal'].upcase == 'GOLD' ? 1 : 0
@@ -145,49 +145,49 @@ describe 'CSV Practice Methods' do
       expect(gold_medal_winners.length).must_equal count
     end
   end
-
+  
   describe 'country_totals_sorted_by_country' do
     it 'orders all the total medal reports' do
       # Arrange
       data = load_data(OLYMPIC_DATA_FILENAME)
       medal_totals = total_medals_per_country(data)
-
+      
       # Act
-       sorted_medal_list = medals_sorted_by_country(medal_totals)
-
+      sorted_medal_list = medals_sorted_by_country(medal_totals)
+      
       # Assert
       last_medal = sorted_medal_list.first
       sorted_medal_list.each do |current_entry|
         expect(last_medal[:country] <= current_entry[:country]).must_equal true
-
+        
         last_medal = current_entry
       end
     end
   end
-
+  
   describe 'total_medals_per_country' do
     it 'returns the right value' do
       # Arrange
       data = load_data(OLYMPIC_DATA_FILENAME)
       medal_totals = total_medals_per_country(data)
-
-
+      
+      
       # Act
       most_medals = country_with_most_medals(medal_totals)
-
+      
       # Assert
       expect(most_medals[:country]).must_equal 'United States'
     end
   end
-
+  
   describe 'athlete_height_in_inches' do
     it 'correctly converts the height to inches' do
       # Arrange
       data = load_data(OLYMPIC_DATA_FILENAME)
-
+      
       # Act
       converted_data = athlete_height_in_inches(data)
-
+      
       # Assert
       expect(converted_data.class).must_equal Array
       converted_data.each_with_index do |entry, index|
